72b5807f944fd25b405212255b071022e25a746a
twojstaryzdomu <twojstaryzdomu@github.com>
2021-08-24 22:09:15 +0200

    Refactored uncompress out of initial url check

--- download_image.sh
+++ download_image.sh
@@ -4,23 +4,28 @@ set -uo pipefail
 case $1 in
     "raspbian_lite:2020-02-13")
         url=https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2020-02-14/2020-02-13-raspbian-buster-lite.zip
-        uncompress="unzip -u"
     ;;
     "raspios_lite:2021-03-04")
         url=https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2021-03-25/2021-03-04-raspios-buster-armhf-lite.zip
-        uncompress="unzip -u"
     ;;
     "raspios_lite:2021-05-07")
         url=https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2021-05-28/2021-05-07-raspios-buster-armhf-lite.zip
-        uncompress="unzip -u"
     ;;
     "dietpi:rpi_armv6_buster")
         url=https://dietpi.com/downloads/images/DietPi_RPi-ARMv6-Buster.7z
-        uncompress="7zr e"
     ;;
     *)
     echo "Unknown image $1"
     exit 1
+    ;;
+esac
+
+case $url in
+    *.zip)
+        uncompress="unzip -u"
+    ;;
+    *.7z)
+        uncompress="7zr e"
     ;;
 esac
 
