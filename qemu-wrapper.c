7f593f3e161182d7c0786b377fc5019ccd23e890
Paul Guyot <pguyot@kallisys.net>
2022-10-09 00:45:07 +0200

    Run on ubuntu 22.04

--- qemu-wrapper.c
+++ qemu-wrapper.c
@@ -12,7 +12,7 @@
 
 int main(int argc, char **argv, char **envp) {
 	int this_len = strlen(argv[0]);
-	char staticpath[this_len + 2];
+	char staticpath[this_len];
 	char *newargv[argc + 3];
 
 	newargv[0] = argv[0];
@@ -21,7 +21,7 @@ int main(int argc, char **argv, char **envp) {
 
 	memcpy(&newargv[3], &argv[1], sizeof(*argv) * (argc -1));
 	newargv[argc + 2] = NULL;
-	strncpy(staticpath, argv[0], this_len + 2);
-	staticpath[this_len] = '0';
+	memcpy(staticpath, argv[0], this_len - 1);
+	staticpath[this_len - 1] = 0;
 	return execve(staticpath, newargv, envp);
 }
