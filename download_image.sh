7d9af3771b7a8410921dc67794129920eb28be46
Paul Guyot <pguyot@kallisys.net>
2023-08-10 10:59:27 +0200

    Fixup

--- download_image.sh
+++ download_image.sh
@@ -74,10 +74,10 @@ mkdir -p ${tempdir}
 cd ${tempdir}
 case ${url} in
     file://localhost/*)
-        cp ${url#file://localhost/} .
+        cp ${url#file://localhost} .
     ;;
     file:///*)
-        cp ${url#file:///} .
+        cp ${url#file://} .
     ;;
     https:/*|http:/*)
         wget --trust-server-names --content-disposition -q ${url}
